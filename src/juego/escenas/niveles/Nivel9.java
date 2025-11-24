117247309d8eac3549d48a1551d6c1571e2f57c5
Ana <silva05anagonzalez@gmail.com>
2025-11-23 22:08:13 -0600

    Update Nivel9.java

--- src/juego/escenas/niveles/Nivel9.java
+++ src/juego/escenas/niveles/Nivel9.java
@@ -11,7 +11,7 @@ import juego.escenas.dificultadNiveles.NivelDificil;
 
 public class Nivel9 extends NivelDificil {
 	public Nivel9() {
-		super(10, 10);
+		super(20, 30);
 		super.setNivelActual(9);
 	}
 }
